b91455921fe2c85f43b2d7c20a48c34cd0b1a9ae
bianavic <bianavic@gmail.com>
2023-11-10 15:48:32 -0300

    initialize project

---
+++ src/main/java/org/edu/fabs/javabankservice/JavaBankServiceApplication.java
@@ -0,0 +1,13 @@
+package org.edu.fabs.javabankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBankServiceApplication.class, args);
+	}
+
+}
